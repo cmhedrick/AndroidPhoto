d7d0de35d00b8273cc23d5854fda7cd7b99d28d8
cmhedrick <christopher.m.hedrick@gmail.com>
2017-06-18 22:36:10 -0400

    Initial commit DAY 18/100

---
+++ AndroidPhoto/MainActivity.cs
@@ -0,0 +1,96 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using System.Collections.Generic;
+using Android.Content;
+using Android.Content.PM;
+using Android.Graphics;
+using Android.Provider;
+using Java.IO;
+using Environment = Android.OS.Environment;
+using Uri = Android.Net.Uri;
+
+namespace AndroidPhoto
+{
+    [Activity(Label = "AndroidPhoto", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private ImageView _imageView;
+
+        public static class App
+        {
+            public static File _file;
+            public static File _dir;
+            public static Bitmap bitmap;
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            Intent mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
+            Uri contentUri = Uri.FromFile(App._file);
+            mediaScanIntent.SetData(contentUri);
+            SendBroadcast(mediaScanIntent);
+
+            // We call the BitmapHelper function LoadAndResizeBitmap, to resize the image
+            // if image is fully rendered it could cause the app to crash
+            int height = Resources.DisplayMetrics.HeightPixels;
+            int width = _imageView.Height;
+            App.bitmap = App._file.Path.LoadAndResizeBitmap(width, height);
+            if (App.bitmap != null)
+            {
+                _imageView.SetImageBitmap(App.bitmap);
+                App.bitmap = null;
+            }
+
+            // Perform garbae collection on image
+            GC.Collect();
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            if (IsThereAnAppToTakePictures())
+            {
+                CreateDirectoryForPictures();
+
+                Button button = FindViewById<Button>(Resource.Id.takePhotoButton);
+                _imageView = FindViewById<ImageView>(Resource.Id.imageView);
+                button.Click += TakeAPicture;
+            }
+        }
+
+        private void CreateDirectoryForPictures()
+        {
+            App._dir = new File(
+                Environment.GetExternalStoragePublicDirectory(
+                    Environment.DirectoryPictures), "AndroidPhoto");
+            if (!App._dir.Exists())
+            {
+                App._dir.Mkdirs();
+            }
+        }
+
+        private bool IsThereAnAppToTakePictures()
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            IList<ResolveInfo> availableActivities =
+                PackageManager.QueryIntentActivities(intent, PackageInfoFlags.MatchDefaultOnly);
+            return availableActivities != null && availableActivities.Count > 0;
+        }
+
+        private void TakeAPicture(object sender, EventArgs eventArgs)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            App._file = new File(App._dir, String.Format("AndroidPhoto_{0}.jpg", Guid.NewGuid()));
+            intent.PutExtra(MediaStore.ExtraOutput, Uri.FromFile(App._file));
+            StartActivityForResult(intent, 0);
+        }
+    }
+}
+
